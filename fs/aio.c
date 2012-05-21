b73d45719bc8ae8969cf17c2183efa038da61875
Linus Torvalds <torvalds@linux-foundation.org>
2012-05-21 16:06:20 -0700

    vfs: make AIO use the proper rw_verify_area() area helpers
    We had for some reason overlooked the AIO interface, and it didn't use
    the proper rw_verify_area() helper function that checks (for example)
    mandatory locking on the file, and that the size of the access doesn't
    cause us to overflow the provided offset limits etc.
    
    Instead, AIO did just the security_file_permission() thing (that
    rw_verify_area() also does) directly.
    
    This fixes it to do all the proper helper functions, which not only
    means that now mandatory file locking works with AIO too, we can
    actually remove lines of code.
    
    Bug: 30609570
    
    Change-Id: I3ad4b846f19c60cb12224020cd20c9ee7c761f23
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-1818
    Reported-by: Manish Honap <manish_honap_vit@yahoo.co.in>
    Cc: stable@vger.kernel.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Erwan Mahe <erwan.mahe@intel.com>
    Reviewed-on: https://android.intel.com/539337
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Tested-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Dubray, SimonX <simonx.dubray@intel.com>
    Reviewed-by: Maalem, Saadi <saadi.maalem@intel.com>

--- fs/aio.c
+++ fs/aio.c
@@ -970,30 +970,34 @@ static ssize_t aio_setup_vectored_rw(int rw, struct kiocb *kiocb, bool compat)
 	if (ret < 0)
 		return ret;
 
-	/* ki_nbytes now reflect bytes instead of segs */
+	ret = rw_verify_area(rw, kiocb->ki_filp, &kiocb->ki_pos, ret);
+	if (ret < 0)
+		return ret;
+
+	kiocb->ki_nr_segs = kiocb->ki_nbytes;
+	kiocb->ki_cur_seg = 0;
+	/* ki_nbytes/left now reflect bytes instead of segs */
 	kiocb->ki_nbytes = ret;
 	return 0;
 }
 
-static ssize_t aio_setup_single_vector(int rw, struct kiocb *kiocb)
+static ssize_t aio_setup_single_vector(int type, struct file *file, struct kiocb *kiocb)
 {
-	size_t len = kiocb->ki_nbytes;
+	int bytes;
 
-	if (len > MAX_RW_COUNT)
-		len = MAX_RW_COUNT;
-
-	if (unlikely(!access_ok(!rw, kiocb->ki_buf, len)))
-                return -EFAULT;
+	bytes = rw_verify_area(type, file, &kiocb->ki_pos, kiocb->ki_left);
+	if (bytes < 0)
+		return bytes;
 
 	kiocb->ki_iovec = &kiocb->ki_inline_vec;
 	kiocb->ki_iovec->iov_base = kiocb->ki_buf;
-	kiocb->ki_iovec->iov_len = len;
+	kiocb->ki_iovec->iov_len = bytes;
 	kiocb->ki_nr_segs = 1;
 	return 0;
 }
 
 /*
- * aio_setup_iocb:
+ * aio_run_iocb:
  *	Performs the initial checks and aio retry method
  *	setup for the kiocb at the time of io submission.
  */
@@ -1007,29 +1011,45 @@ static ssize_t aio_run_iocb(struct kiocb *req, bool compat)
 
 	switch (req->ki_opcode) {
 	case IOCB_CMD_PREAD:
-	case IOCB_CMD_PREADV:
-		mode	= FMODE_READ;
-		rw	= READ;
-		rw_op	= file->f_op->aio_read;
-		goto rw_common;
-
+		ret = -EBADF;
+		if (unlikely(!(file->f_mode & FMODE_READ)))
+			break;
+		ret = -EFAULT;
+		if (unlikely(!access_ok(VERIFY_WRITE, req->ki_buf,
+			req->ki_left)))
+			break;
+		ret = aio_setup_single_vector(READ, file, req);
+		if (ret)
+			break;
+		ret = -EINVAL;
+		break;
 	case IOCB_CMD_PWRITE:
+		ret = -EBADF;
+		if (unlikely(!(file->f_mode & FMODE_WRITE)))
+			break;
+		ret = -EFAULT;
+		if (unlikely(!access_ok(VERIFY_READ, req->ki_buf,
+			req->ki_left)))
+			break;
+		ret = aio_setup_single_vector(WRITE, file, req);
+		if (ret)
+			break;
+		ret = -EINVAL;
+		break;
+	case IOCB_CMD_PREADV:
+		ret = -EBADF;
+		if (unlikely(!(file->f_mode & FMODE_READ)))
+			break;
+		ret = aio_setup_vectored_rw(READ, req, compat);
+		if (ret)
+			break;
+		ret = -EINVAL;
+		break;
 	case IOCB_CMD_PWRITEV:
-		mode	= FMODE_WRITE;
-		rw	= WRITE;
-		rw_op	= file->f_op->aio_write;
-		goto rw_common;
-rw_common:
-		if (unlikely(!(file->f_mode & mode)))
-			return -EBADF;
-
-		if (!rw_op)
-			return -EINVAL;
-
-		ret = (req->ki_opcode == IOCB_CMD_PREADV ||
-		       req->ki_opcode == IOCB_CMD_PWRITEV)
-			? aio_setup_vectored_rw(rw, req, compat)
-			: aio_setup_single_vector(rw, req);
+		ret = -EBADF;
+		if (unlikely(!(file->f_mode & FMODE_WRITE)))
+			break;
+		ret = aio_setup_vectored_rw(WRITE, req, compat);
 		if (ret)
 			return ret;
 
