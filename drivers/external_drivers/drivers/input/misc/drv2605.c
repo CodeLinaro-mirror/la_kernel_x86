b07326ab3ca34ea3b75511850e32e12b9c749ad6
Victor Tasayco Loarte <victorx.tasayco.loarte@intel.com>
2016-03-18 15:12:51 +0100

    misc/drv2605: Fixup security issues with data access
    Bug: 25981545
    Bug: 27634138
    
    Change-Id: Ifa2d926b8cbd58e80c266b914144c6fa4ae58798
    Tracked-On: https://jira.ndg.intel.com/browse/MARVIN-1570
    Signed-off-by: Victor Tasayco Loarte <victorx.tasayco.loarte@intel.com>
    Reviewed-on: https://android.intel.com/483671
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>

--- drivers/external_drivers/drivers/input/misc/drv2605.c
+++ drivers/external_drivers/drivers/input/misc/drv2605.c
@@ -610,14 +610,28 @@ static struct timed_output_dev to_dev = {
 
 static char read_val;
 
-static ssize_t drv260x_read(struct file *filp, char *buff, size_t length, loff_t *offset)
+static ssize_t drv260x_read(struct file *filp, char __user *buff, size_t length,
+				loff_t *offset)
 {
-	buff[0] = read_val;
-	return 1;
+	size_t tocopy = min(length, sizeof(read_val));
+
+	if (copy_to_user(buff, &read_val, tocopy))
+		return -EFAULT;
+
+	return tocopy;
 }
 
-static ssize_t drv260x_write(struct file *filp, const char *buff, size_t len, loff_t *off)
+static ssize_t drv260x_write(struct file *filp, const char __user *buff, size_t len,
+				loff_t *off)
 {
+	char cmdid;
+
+	if (len < 1)
+		return -EINVAL;
+
+	if (copy_from_user(&cmdid, buff, 1))
+		return -EFAULT;
+
 	mutex_lock(&vibdata.lock);
 	hrtimer_cancel(&vibdata.timer);
 
@@ -630,12 +644,13 @@ static ssize_t drv260x_write(struct file *filp, const char *buff, size_t len, lo
 		drv260x_change_mode(MODE_STANDBY);
 	}
 
-	switch (buff[0]) {
+	switch (cmdid) {
 	case HAPTIC_CMDID_PLAY_SINGLE_EFFECT:
 	case HAPTIC_CMDID_PLAY_EFFECT_SEQUENCE:
 	{
 		memset(&vibdata.sequence, 0, sizeof(vibdata.sequence));
-		if (!copy_from_user(&vibdata.sequence, &buff[1], len - 1)) {
+		if (!copy_from_user(&vibdata.sequence, &buff[1], min(len - 1,
+			sizeof(vibdata.sequence)))) {
 			vibdata.should_stop = NO;
 			wake_lock(&vibdata.wklock);
 			schedule_work(&vibdata.work_play_eff);
@@ -644,12 +659,23 @@ static ssize_t drv260x_write(struct file *filp, const char *buff, size_t len, lo
 	}
 	case HAPTIC_CMDID_PLAY_TIMED_EFFECT:
 	{
+		char data[2] = {0, 0};
 		unsigned int value = 0;
 		char mode;
 
-		value = buff[2];
+		if (len < (sizeof(data) + 1)) {
+			len = -EINVAL;
+			break;
+		}
+
+		if (copy_from_user(&data, &buff[1], sizeof(data))) {
+			len = -EFAULT;
+			break;
+		}
+
+		value = data[1];
 		value <<= 8;
-		value |= buff[1];
+		value |= data[0];
 
 		if (value) {
 			wake_lock(&vibdata.wklock);
