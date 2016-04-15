8bf5330c2265ce9d14e0955410540b9b2d493cd4
Victor Tasayco Loarte <victorx.tasayco.loarte@intel.com>
2016-04-15 10:48:17 +0200

    intel-mid: make get_device_id() overridable
    Change-Id: Iee3f04580c91f1a9d91ce477b37aa277b0f08814
    Tracked-On: https://jira01.devtools.intel.com/browse/IAWP-71
    Signed-off-by: Victor Tasayco Loarte <victorx.tasayco.loarte@intel.com>
    Reviewed-on: https://android.intel.com/495115
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Tested-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>

--- arch/x86/platform/intel-mid/intel_mid_sfi.c
+++ arch/x86/platform/intel-mid/intel_mid_sfi.c
@@ -493,7 +493,7 @@ static void __init sfi_handle_sd_dev(struct sfi_device_table_entry *pentry,
 	sd_info.platform_data = pdata;
 }
 
-struct devs_id __init *get_device_id(u8 type, char *name)
+struct devs_id __init  __attribute__((weak)) *get_device_id(u8 type, char *name)
 {
 	struct devs_id *dev = device_ids;
 
