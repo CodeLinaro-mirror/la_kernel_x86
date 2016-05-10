e173512b1be6208d1d7eaeaf7faa0b0cc1f8a114
Hu Bingquan <bingquan.hu@intel.com>
2016-05-10 16:31:26 +0800

    sensor: add proximity sensor regulator controlled power rail
    add power on/off function support anthracite
    
    Change-Id: I236055bbfd6f731e5568badd7c610d13676d377a
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-4
    Signed-off-by: Hu Bingquan <bingquan.hu@intel.com>
    Signed-off-by: Kai Qiang <kaix.qiang@intel.com>
    Reviewed-on: https://android.intel.com/504431
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Tested-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Maalem, Saadi <saadi.maalem@intel.com>
    Reviewed-by: Ledentec, AlexandreX <alexandrex.ledentec@intel.com>
    Tested-by: Ledentec, AlexandreX <alexandrex.ledentec@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_tmd26723.c
+++ arch/x86/platform/intel-mid/device_libs/platform_tmd26723.c
@@ -11,16 +11,75 @@
 #include <linux/platform_device.h>
 #include <linux/input/tmd26723.h>
 #include <asm/intel-mid.h>
+#include <linux/regulator/consumer.h>
 #include "platform_tmd26723.h"
+
+static struct regulator *vprog2_reg;
+#define VPROG2_VAL 2850000
+
+/*
+ * PMIC regulator vprog2 init
+ */
+static int tmd26723_platform_init(struct device *dev)
+{
+	int ret;
+
+	vprog2_reg = regulator_get(dev, "vprog2");
+	if (IS_ERR(vprog2_reg)) {
+		dev_err(dev, "regulator_get failed\n");
+		return PTR_ERR(vprog2_reg);
+	}
+
+	ret = regulator_set_voltage(vprog2_reg, VPROG2_VAL, VPROG2_VAL);
+	if (ret) {
+		dev_err(dev, "regulator voltage set failed\n");
+		regulator_put(vprog2_reg);
+	}
+	return ret;
+}
+
+/*
+ * PMIC regulator vprog2 de-init
+ */
+static int tmd26723_platform_exit(struct device *dev)
+{
+	regulator_put(vprog2_reg);
+	return 0;
+}
+
+static int tmd26723_power_on(struct device *dev)
+{
+	int ret;
+
+	ret = regulator_enable(vprog2_reg);
+	if (ret < 0) {
+		dev_err(dev, "%s:is failed\n", __func__);
+		return ret;
+	}
+	return 0;
+}
+
+static int tmd26723_power_off(struct device *dev)
+{
+	int ret;
+
+	ret = regulator_disable(vprog2_reg);
+	if (ret < 0) {
+		dev_err(dev, "%s:is failed\n", __func__);
+		return ret;
+	}
+	return 0;
+}
+
 void *tmd26723_ps_platform_data(void *info)
 {
 	static struct proximity_sensor_platform_data proximity_sensor_pdata;
 
 	proximity_sensor_pdata.gpio_int = get_gpio_by_name("prox_int_n");
-	proximity_sensor_pdata.init = NULL;
-	proximity_sensor_pdata.exit = NULL;
-	proximity_sensor_pdata.power_on = NULL;
-	proximity_sensor_pdata.power_off = NULL;
+	proximity_sensor_pdata.init = tmd26723_platform_init;
+	proximity_sensor_pdata.exit = tmd26723_platform_exit;
+	proximity_sensor_pdata.power_on = tmd26723_power_on;
+	proximity_sensor_pdata.power_off = tmd26723_power_off;
 
 	return &proximity_sensor_pdata;
 }
