95402e8785a1d4ccc9720c783f64b118a39ef9e8
Sophia Gong <sophia.gong@intel.com>
2016-05-12 10:39:43 +0800

    display: change tianma panel active area size as 38x34
    This patch is to correct tianma x/ydpi value to 240.
    
    Change-Id: Id9207f8898d02c2a58e8daa4bf1350397d9f2f3f
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-595
    Signed-off-by: Sophia Gong <sophia.gong@intel.com>
    Reviewed-on: https://android.intel.com/505256
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Liu, WeiX W <weix.w.liu@intel.com>
    Reviewed-by: Maalem, Saadi <saadi.maalem@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-by: Hadjimegrian, MathieuX <mathieux.hadjimegrian@intel.com>

--- drivers/external_drivers/intel_media/display/tng/drv/tianma_cmd.c
+++ drivers/external_drivers/intel_media/display/tng/drv/tianma_cmd.c
@@ -683,8 +683,8 @@ void tianma_cmd_get_panel_info(int pipe,
 	PSB_DEBUG_ENTRY("\n");
 
 	if (pipe == 0) {
-		pi->width_mm = 40;
-		pi->height_mm = 40;
+		pi->width_mm = 38;
+		pi->height_mm = 34;
 	}
 }
 
