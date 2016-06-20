8d23dc84421849878d7f301f7499d98f8d24bdf2
Julien Masson <julienx.masson@intel.com>
2016-06-20 14:54:58 +0800

    i2c-designware: add module parameter for SDA hold time
    The parameter hold_time is introduced to set the hold time of SDA during
    i2c data transfer.
    This parameter is read only and can be set in the command line.
    The default value is 0x05 according to the soc reference manual.
    
    Example:
    add in the command line i2c_designware_core.hold_time=40
    (1 / 166 MHz) * 40 = 240 ns of hold time
    
    Change-Id: I45ca0cfc632cf4fdde4624700e56ff2820d5d582
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-921
    Signed-off-by: Julien Masson <julienx.masson@intel.com>
    Reviewed-on: https://android.intel.com/522595
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-by: Slimani, Kamel <kamel.slimani@intel.com>

--- drivers/external_drivers/drivers/i2c/busses/i2c-designware-core.c
+++ drivers/external_drivers/drivers/i2c/busses/i2c-designware-core.c
@@ -55,6 +55,10 @@ void i2c_dw_clear_int(struct dw_i2c_dev *dev);
 static void dw_i2c_acpi_setup(struct device *pdev, struct dw_i2c_dev *dev);
 u32 i2c_dw_read_comp_param(struct dw_i2c_dev *dev);
 
+static u32 hold_time = DEFAULT_I2C_HOLD_TIME;
+module_param(hold_time, uint, 0400);
+MODULE_PARM_DESC(hold_time, "SDA hold time in units of ic_clk period");
+
 static char *abort_sources[] = {
 	[ABRT_7B_ADDR_NOACK] =
 		"slave address not acknowledged (7bit mode)",
@@ -1085,6 +1089,9 @@ int i2c_dw_init(struct dw_i2c_dev *dev)
 
 	/* configure the i2c master */
 	dw_writel(dev, dev->master_cfg , DW_IC_CON);
+
+	/* set IC_SDA_HOLD to hold_time */
+	dw_writel(dev, hold_time, DW_IC_SDA_HOLD);
 
 	return 0;
 }
