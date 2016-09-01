42f53e7b3c1ef7d6721862ce662b6d728e3a5f4a
Simon Dubray <simonx.dubray@intel.com>
2016-09-01 15:45:28 +0200

    net: wireless: bcmdhd: fix 2 memory leaks
    This patch makes sure we free the structure pointed by bus->sih
    and ((si_info_t *)bus->sih)->cores_info
    kmemleak frequently complains for these 2 memory leaks:
    
    unreferenced object 0xffff8800091ae300 (size 256):
      comm "WifiStateMachin", pid 621, jiffies 4296455689 (age 869.950s)
      backtrace:
        [<ffffffff829c2449>] kmemleak_alloc+0x49/0xb0
        [<ffffffff821817ef>] __kmalloc+0x12f/0x2c0
        [<ffffffff8243ba6c>] osl_malloc+0x6c/0xa0
        [<ffffffff8243baae>] osl_mallocz+0xe/0x30
        [<ffffffff8243fd12>] si_attach+0x32/0x110
        [<ffffffff8248308b>] dhdsdio_probe_attach+0x43b/0xd20
        [<ffffffff8248dc0d>] dhd_bus_devreset+0x16d/0x2e0
        [<ffffffff8242d098>] dhd_net_bus_devreset+0x48/0xf0
        [<ffffffff82444372>] wl_android_wifi_on+0xe2/0x160
        [<ffffffff82430e89>] dhd_open+0xb9/0x270
    
    unreferenced object 0xffff880010154000 (size 2048):
      comm "WifiStateMachin", pid 621, jiffies 4296455689 (age 870.010s)
      backtrace:
        [<ffffffff829c2449>] kmemleak_alloc+0x49/0xb0
        [<ffffffff821817ef>] __kmalloc+0x12f/0x2c0
        [<ffffffff8243ba6c>] osl_malloc+0x6c/0xa0
        [<ffffffff8243baae>] osl_mallocz+0xe/0x30
        [<ffffffff8243fd2b>] si_attach+0x4b/0x110
        [<ffffffff8248308b>] dhdsdio_probe_attach+0x43b/0xd20
        [<ffffffff8248dc0d>] dhd_bus_devreset+0x16d/0x2e0
        [<ffffffff8242d098>] dhd_net_bus_devreset+0x48/0xf0
        [<ffffffff82444372>] wl_android_wifi_on+0xe2/0x160
        [<ffffffff82430e89>] dhd_open+0xb9/0x270
    
    Change-Id: I3c09eb692bf996368cac4b3f1eb71ba89d2187c0
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-1729
    Signed-off-by: Simon Dubray <simonx.dubray@intel.com>
    Reviewed-on: https://android.intel.com/537341
    Reviewed-by: Nassiet, GaelleX <gaellex.nassiet@intel.com>
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Ranquet, Guillaume <guillaume.ranquet@intel.com>
    Reviewed-by: Crippa, Roberto <roberto.crippa@intel.com>
    Reviewed-by: Ben Abdessalem, AmrX <amrx.ben.abdessalem@intel.com>
    Tested-by: Ben Abdessalem, AmrX <amrx.ben.abdessalem@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Maalem, Saadi <saadi.maalem@intel.com>

--- drivers/net/wireless/bcmdhd/dhd_pcie.c
+++ drivers/net/wireless/bcmdhd/dhd_pcie.c
@@ -269,6 +269,7 @@ dhd_bus_t* dhdpcie_bus_attach(osl_t *osh, volatile char* regs, volatile char* tc
 		bus->tcm = tcm;
 		bus->tcm_size = tcm_size;
 		bus->osh = osh;
+		bus->sih = NULL;
 
 		dll_init(&bus->const_flowring);
 
@@ -454,7 +455,11 @@ dhdpcie_dongle_attach(dhd_bus_t *bus)
 
 
 	bus->alp_only = TRUE;
-	bus->sih = NULL;
+
+	if (bus->sih) {
+		si_detach(bus->sih);
+		bus->sih = NULL;
+	}
 
 	/* Set bar0 window to si_enum_base */
 	dhdpcie_bus_cfg_set_bar0_win(bus, SI_ENUM_BASE);
