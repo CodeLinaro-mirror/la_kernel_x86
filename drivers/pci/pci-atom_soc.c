e7ac165ecbe7e73a32c7ffc6c31ecc0de962ed5a
Simon Dubray <simonx.dubray@intel.com>
2016-10-10 15:40:03 +0200

    pci-atom_soc: add need_resume callback
    need_resume callback is needed in struct pci_platform_pm_ops
    else we have a panic during suspend.
    
    Change-Id: Ic973ffab5c21782cb823224e72cb68f9cd3a4afe
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-2212
    Signed-off-by: Simon Dubray <simonx.dubray@intel.com>
    Reviewed-on: https://android.intel.com/544472
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Lachaud, EtienneX <etiennex.lachaud@intel.com>
    Reviewed-by: Korpershoek, MattijsX <mattijsx.korpershoek@intel.com>
    Reviewed-by: Hadjimegrian, MathieuX <mathieux.hadjimegrian@intel.com>
    Reviewed-by: Wei Yu Neng, Loic <loic.wei.yu.neng@intel.com>

--- drivers/pci/pci-atom_soc.c
+++ drivers/pci/pci-atom_soc.c
@@ -43,13 +43,18 @@ static int mid_pci_run_wake(struct pci_dev *dev, bool enable)
 	return 0;
 }
 
+static pci_power_t mid_pci_need_resume(struct pci_dev *pdev)
+{
+	return false;
+}
+
 static struct pci_platform_pm_ops mid_pci_platform_pm = {
 	.is_manageable = mid_pci_power_manageable,
 	.choose_state = mid_pci_choose_state,
 	.sleep_wake = mid_pci_sleep_wake,
 	.run_wake = mid_pci_run_wake,
 	.set_state = pmu_pci_set_power_state,
-	.choose_state = pmu_pci_choose_state,
+	.need_resume = mid_pci_need_resume,
 };
 
 /**
