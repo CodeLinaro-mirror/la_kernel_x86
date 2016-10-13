bb859a41649fba787ba3ba6212cf45d0fadb29f7
Habib BEL HAJ ALI <habibx.bel.aj.ali@intel.com>
2016-10-13 11:56:59 +0200

    thermal: port iio_basincove_gpadc driver changes from kernel 3.10
    SHA1: 21ce98507dc7de3fb9d6d17fb9a7b684302df9ac
    
    Change-Id: I7a517010c2f67f22b98ef399c605ed0e77d298ce
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-2289
    Signed-off-by: Habib BEL HAJ ALI <habibx.bel.aj.ali@intel.com>
    Reviewed-on: https://android.intel.com/545203
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Wei Yu Neng, Loic <loic.wei.yu.neng@intel.com>

--- arch/x86/platform/intel-mid/device_libs/platform_bcove_adc.c
+++ arch/x86/platform/intel-mid/device_libs/platform_bcove_adc.c
@@ -17,23 +17,43 @@
 #include <linux/iio/types.h>
 #include <asm/intel-mid.h>
 #include <asm/intel_basincove_gpadc.h>
-#include <linux/platform_data/intel_mid_remoteproc.h>
+#include <asm/intel_mid_remoteproc.h>
 
 #include "platform_bcove_adc.h"
+#include "platform_ipc.h"
 
 /* SRAM address where the GPADC interrupt register is cached */
 #define GPADC_SRAM_INTR_ADDR	0xfffff615
 
-static struct gpadc_regmap_t basincove_gpadc_regmaps[GPADC_CH_NUM] = {
-	{"VBAT",        5, 0xE9, 0xEA, },
-	{"BATID",       4, 0xEB, 0xEC, },
-	{"IBAT",        5, 0xED, 0xEE, },
-	{"PMICTEMP",    3, 0xCC, 0xCD, },
-	{"BATTEMP0",    2, 0xC8, 0xC9, },
-	{"BATTEMP1",    2, 0xCA, 0xCB, },
-	{"SYSTEMP0",    3, 0xC2, 0xC3, },
-	{"SYSTEMP1",    3, 0xC4, 0xC5, },
-	{"SYSTEMP2",    3, 0xC6, 0xC7, },
+/* Order: Name, Control-bit, result-MSB, result-LSB,
+ *        alert-min-MSB, alert-min-LSB, alert-max-MSB, alert-max-LSB.
+ * 0xFF used for when any of these is NA.
+ */
+static struct gpadc_regmap_t basincove_gpadc_regmaps[BCOVE_GPADC_CH_NUM] = {
+	{"VBAT",        5, 0xE9, 0xEA, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATID",       4, 0xEB, 0xEC, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"IBAT",        5, 0xED, 0xEE, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"PMICTEMP",    3, 0xCC, 0xCD, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATTEMP0",    2, 0xC8, 0xC9, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATTEMP1",    2, 0xCA, 0xCB, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"SYSTEMP0",    3, 0xC2, 0xC3, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"SYSTEMP1",    3, 0xC4, 0xC5, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"SYSTEMP2",    3, 0xC6, 0xC7, 0xFF, 0xFF, 0xFF, 0xFF},
+};
+
+static struct gpadc_regmap_t shadycove_gpadc_regmaps[SCOVE_GPADC_CH_NUM] = {
+	{"VBAT",        5, 0xE9, 0xEA, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATID",       4, 0xEC, 0xED, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"PMICTEMP",    3, 0xD5, 0xD6, 0xC7, 0xC8, 0xC7, 0xC8},
+	{"BATTEMP0",    2, 0xD1, 0xD2, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"BATTEMP1",    2, 0xD3, 0xD4, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"SYSTEMP0",    3, 0xCB, 0xCC, 0xB7, 0xB8, 0xB9, 0xBA},
+	{"SYSTEMP1",    3, 0xCD, 0xCE, 0xBB, 0xBC, 0xBD, 0xBE},
+	{"SYSTEMP2",    3, 0xCF, 0xD0, 0xBF, 0xC0, 0xC1, 0xC2},
+	{"USBID",       1, 0xEE, 0xEF, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"PEAK",        7, 0xF7, 0xF8, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"AGND",	6, 0xF0, 0xF1, 0xFF, 0xFF, 0xFF, 0xFF},
+	{"VREF",	6, 0xF0, 0xF1, 0xFF, 0xFF, 0xFF, 0xFF},
 };
 
 static struct gpadc_regs_t basincove_gpadc_regs = {
@@ -43,6 +63,17 @@ static struct gpadc_regs_t basincove_gpadc_regs = {
 	.mirqlvl1		= 0x0C,
 	.mirqlvl1_adc		= (1 << 4),
 	.adc1cntl		= 0xDD,
+	.adcirq			= 0x06,
+	.madcirq		= 0x11,
+};
+
+static struct gpadc_regs_t shadycove_gpadc_regs = {
+	.gpadcreq		= 0xDC,
+	.gpadcreq_irqen		= 0,
+	.gpadcreq_busy		= (1 << 0),
+	.mirqlvl1		= 0x0C,
+	.mirqlvl1_adc		= (1 << 4),
+	.adc1cntl		= 0xEB,
 	.adcirq			= 0x06,
 	.madcirq		= 0x11,
 };
@@ -70,6 +101,34 @@ struct iio_map basincove_iio_maps[] = {
 	MSIC_ADC_MAP("CH7", "bcove_thrm", "SYSTEMP1"),
 	MSIC_ADC_MAP("CH8", "bcove_thrm", "SYSTEMP2"),
 	MSIC_ADC_MAP("CH3", "bcove_thrm", "PMICTEMP"),
+	MSIC_ADC_MAP("CH6", "THERMAL", "SYSTEMP0"),
+	MSIC_ADC_MAP("CH7", "THERMAL", "SYSTEMP1"),
+	MSIC_ADC_MAP("CH8", "THERMAL", "SYSTEMP2"),
+	MSIC_ADC_MAP("CH3", "THERMAL", "PMICTEMP"),
+	{ },
+};
+
+struct iio_map shadycove_iio_maps[] = {
+	MSIC_ADC_MAP("CH0", "VIBAT", "VBAT"),
+	MSIC_ADC_MAP("CH1", "BATID", "BATID"),
+	MSIC_ADC_MAP("CH2", "PMICTEMP", "PMICTEMP"),
+	MSIC_ADC_MAP("CH3", "BATTEMP", "BATTEMP0"),
+	MSIC_ADC_MAP("CH4", "BATTEMP", "BATTEMP1"),
+	MSIC_ADC_MAP("CH5", "SYSTEMP", "SYSTEMP0"),
+	MSIC_ADC_MAP("CH6", "SYSTEMP", "SYSTEMP1"),
+	MSIC_ADC_MAP("CH7", "SYSTEMP", "SYSTEMP2"),
+	MSIC_ADC_MAP("CH8", "USBID", "USBID"),
+	MSIC_ADC_MAP("CH9", "PEAK", "PEAK"),
+	MSIC_ADC_MAP("CH10", "GPMEAS", "AGND"),
+	MSIC_ADC_MAP("CH11", "GPMEAS", "VREF"),
+	MSIC_ADC_MAP("CH5", "scove_thrm", "SYSTEMP0"),
+	MSIC_ADC_MAP("CH6", "scove_thrm", "SYSTEMP1"),
+	MSIC_ADC_MAP("CH7", "scove_thrm", "SYSTEMP2"),
+	MSIC_ADC_MAP("CH2", "scove_thrm", "PMICTEMP"),
+	MSIC_ADC_MAP("CH5", "THERMAL", "SYSTEMP0"),
+	MSIC_ADC_MAP("CH6", "THERMAL", "SYSTEMP1"),
+	MSIC_ADC_MAP("CH7", "THERMAL", "SYSTEMP2"),
+	MSIC_ADC_MAP("CH2", "THERMAL", "PMICTEMP"),
 	{ },
 };
 
@@ -93,44 +152,61 @@ static const struct iio_chan_spec const basincove_adc_channels[] = {
 	MSIC_ADC_CHANNEL(IIO_TEMP, 8, "CH8"),
 };
 
-static struct intel_basincove_gpadc_platform_data bcove_adc_pdata = {
-	.channel_num = GPADC_CH_NUM,
-	.intr = GPADC_SRAM_INTR_ADDR,
-	.gpadc_iio_maps = basincove_iio_maps,
-	.gpadc_regmaps = basincove_gpadc_regmaps,
-	.gpadc_regs = &basincove_gpadc_regs,
-	.gpadc_channels = basincove_adc_channels,
+static const struct iio_chan_spec const shadycove_adc_channels[] = {
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 0, "CH0"),
+	MSIC_ADC_CHANNEL(IIO_RESISTANCE, 1, "CH1"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 2, "CH2"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 3, "CH3"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 4, "CH4"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 5, "CH5"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 6, "CH6"),
+	MSIC_ADC_CHANNEL(IIO_TEMP, 7, "CH7"),
+	MSIC_ADC_CHANNEL(IIO_RESISTANCE, 8, "CH8"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 9, "CH9"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 10, "CH10"),
+	MSIC_ADC_CHANNEL(IIO_VOLTAGE, 11, "CH11"),
 };
 
-void __init *bcove_adc_platform_data(void *info)
+static struct intel_basincove_gpadc_platform_data bcove_adc_pdata;
+
+void *bcove_adc_device_handler(void *info)
 {
-	struct platform_device *pdev = NULL;
-	struct sfi_device_table_entry *entry = info;
-	int ret;
-
-	pdev = platform_device_alloc(BCOVE_ADC_DEV_NAME, -1);
-
-	if (!pdev) {
-		pr_err("out of memory for SFI platform dev %s\n",
-					BCOVE_ADC_DEV_NAME);
-		goto out;
+	if (INTEL_MID_BOARD(1, PHONE, MRFL) ||
+		INTEL_MID_BOARD(1, TABLET, MRFL)) {
+		bcove_adc_pdata.channel_num = BCOVE_GPADC_CH_NUM;
+		bcove_adc_pdata.intr = GPADC_SRAM_INTR_ADDR;
+		bcove_adc_pdata.intr_mask = MBATTEMP | MSYSTEMP | MBATT
+			| MVIBATT | MCCTICK;
+		bcove_adc_pdata.gpadc_iio_maps = basincove_iio_maps;
+		bcove_adc_pdata.gpadc_regmaps = basincove_gpadc_regmaps;
+		bcove_adc_pdata.gpadc_regs = &basincove_gpadc_regs;
+		bcove_adc_pdata.gpadc_channels = basincove_adc_channels;
+	} else if (INTEL_MID_BOARD(1, PHONE, MOFD) ||
+		INTEL_MID_BOARD(1, TABLET, MOFD)) {
+		bcove_adc_pdata.channel_num = SCOVE_GPADC_CH_NUM;
+		bcove_adc_pdata.intr = GPADC_SRAM_INTR_ADDR;
+		bcove_adc_pdata.intr_mask = MUSBID | MPEAK | MBATTEMP
+			| MSYSTEMP | MBATT | MVIBATT | MGPMEAS | MCCTICK;
+		bcove_adc_pdata.gpadc_iio_maps = shadycove_iio_maps;
+		bcove_adc_pdata.gpadc_regmaps = shadycove_gpadc_regmaps;
+		bcove_adc_pdata.gpadc_regs = &shadycove_gpadc_regs;
+		bcove_adc_pdata.gpadc_channels = shadycove_adc_channels;
 	}
 
-	bcove_adc_pdata.intr = GPADC_SRAM_INTR_ADDR;
-
-	pdev->dev.platform_data = &bcove_adc_pdata;
-
-	ret = platform_device_add(pdev);
-	if (ret) {
-		pr_err("failed to add bcove adc platform device\n");
-		platform_device_put(pdev);
-		goto out;
-	}
-
-	install_irq_resource(pdev, entry->irq);
-
+#ifndef CONFIG_ACPI
 	register_rpmsg_service("rpmsg_bcove_adc", RPROC_SCU,
 				RP_BCOVE_ADC);
+#endif
 out:
 	return &bcove_adc_pdata;
 }
+
+static const struct devs_id bcove_adc_dev_id __initconst = {
+	.name = "bcove_adc",
+	.type = SFI_DEV_TYPE_IPC,
+	.delay = 1,
+	.get_platform_data = &bcove_adc_device_handler,
+	.device_handler = &ipc_device_handler,
+};
+
+sfi_device(bcove_adc_dev_id);
