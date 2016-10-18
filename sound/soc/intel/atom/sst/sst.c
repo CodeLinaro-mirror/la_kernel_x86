c8e398fe99c2c988038e92a8752a305af77e6c52
Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
2016-10-18 17:52:56 +0200

    ASoC: Intel: atom: fix sst_configure_runtime_pm() for pci devices
    According to drivers/pci/pci-driver.c:
    
    | * Unbound PCI devices are always put in D0, regardless of
    | * runtime PM status.  During probe, the device is set to
    | * active and the usage count is incremented.  If the driver
    | * supports runtime PM, it should call pm_runtime_put_noidle(),
    | * or any other runtime PM helper function decrementing the usage
    | * count, in its probe routine and pm_runtime_get_noresume() in
    | * its remove routine.
    
    This patch:
    
    - moves the save of the of the shim registers before suspending the
      device.
    
    - avoids calling pm_runtime_enable() since it is already done
      in the pci subsystem.
    
    - decrements the usage count as required by pci-driver.c
      Note that this has been inspired from:
      - intel_mid_ssp_spi_probe()
      - sdhci_pci_runtime_pm_allow()
    
    Change-Id: I63a4a4b7aefc3312cb7bd16fdd7b0b6482c68a76
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-2336
    Signed-off-by: Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
    Signed-off-by: Sebastien Guiriec <sebastien.guiriec@intel.com>
    Reviewed-on: https://android.intel.com/546417
    Reviewed-by: Dubray, SimonX <simonx.dubray@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>

--- sound/soc/intel/atom/sst/sst.c
+++ sound/soc/intel/atom/sst/sst.c
@@ -387,12 +387,21 @@ void sst_configure_runtime_pm(struct intel_sst_drv *ctx)
 		pm_runtime_set_active(ctx->dev);
 		pm_runtime_enable(ctx->dev);
 		pm_runtime_put_noidle(ctx->dev);
+		sst_save_shim64(ctx, ctx->shim, ctx->shim_regs64);
 	} else {
-		pm_runtime_enable(ctx->dev);
-		pm_runtime_set_active(ctx->dev);
+		/*
+		 * For pci devices:
+		 * - we should save the shim registers before suspending
+		 *   the device
+		 * - the power management is enabled by the subsystem.
+		 *   So no need to enable here.
+		 * - we must call a pm_runtime_put* as stated
+		 *   in pci-driver.c
+		 */
+		sst_save_shim64(ctx, ctx->shim, ctx->shim_regs64);
+		pm_runtime_put_noidle(ctx->dev);
+		pm_runtime_allow(ctx->dev);
 	}
-
-	sst_save_shim64(ctx, ctx->shim, ctx->shim_regs64);
 }
 EXPORT_SYMBOL_GPL(sst_configure_runtime_pm);
 
