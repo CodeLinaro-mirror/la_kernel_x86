d26be4edbae141dd6c271486ab31e1fea19318c5
Simon Dubray <simonx.dubray@intel.com>
2016-12-14 13:53:30 +0100

    net: wireless: bcmdhd: move wlan mac address path to /device_config
    As /config needs to be the configfs mount point, our old /config
    will now be mounted on /device_config so the wlan mac address path changes.
    
    Change-Id: I53493527ec55bdd0194dab618c9097fc8226552e
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-2935
    Signed-off-by: Simon Dubray <simonx.dubray@intel.com>
    Reviewed-on: https://android.intel.com/557674
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Korpershoek, MattijsX <mattijsx.korpershoek@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Ledentec, AlexandreX <alexandrex.ledentec@intel.com>
    Reviewed-by: Akue, LoicX <loicx.akue@intel.com>

--- drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
+++ drivers/net/wireless/bcmdhd/dhd_linux_platdev.c
@@ -148,7 +148,7 @@ static int wifi_get_external_mac_addr(unsigned char *buf)
 	int i;
 	struct file *fp = NULL;
 	unsigned char c_mac[MAC_ADDRESS_LEN];
-	char fname[] = "/config/wifi/mac.txt";
+	char fname[] = "/device_config/wifi/mac.txt";
 
 	DHD_TRACE(("%s Enter\n", __func__));
 
