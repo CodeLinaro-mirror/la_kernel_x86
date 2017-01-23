56baf88a19572b77f9a993d7c236cde65e23777b
Habib BEL HAJ ALI <habibx.bel.aj.ali@intel.com>
2017-01-23 17:35:09 +0100

    i2c: designware: fix RX FIFO overrun
    This patch is a port from kernel 3.10.
    Original SHA1: 9121a9234577270a58860be4cd576fae677064bc
    
    i2c_dw_xfer_msg() pushes a number of bytes to transmit/receive
    to/from the bus into the TX FIFO.
    For master-rx transactions, the maximum amount of data that can be
    received is calculated depending solely on TX and RX FIFO load.
    
    This is racy - TX FIFO may contain master-rx data yet to be
    processed, which will eventually land into the RX FIFO. This
    data is not taken into account and the function may request more
    data than the controller is actually capable of storing.
    
    This patch ensures the driver takes into account the outstanding
    master-rx data in TX FIFO to prevent RX FIFO overrun.
    
    
    Change-Id: If9a725393ab5ff52b33c7bb6b586dd7a196345f4
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-3947
    Signed-off-by: Josef Ahmad <josef.ahmad@linux.intel.com>
    Acked-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    Cc: stable@kernel.org
    Reviewed-on: https://android.intel.com/509486
    Reviewed-by: Wei Yu Neng, Loic <loic.wei.yu.neng@intel.com>
    Reviewed-by: Bel Aj Ali, HabibX <habibx.bel.aj.ali@intel.com>
    Tested-by: Bel Aj Ali, HabibX <habibx.bel.aj.ali@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
    Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
    Reviewed-on: https://android.intel.com/564703
    Reviewed-by: Dubray, SimonX <simonx.dubray@intel.com>
    Reviewed-by: Lachaud, EtienneX <etiennex.lachaud@intel.com>

--- drivers/i2c/busses/i2c-designware-core.h
+++ drivers/i2c/busses/i2c-designware-core.h
@@ -213,6 +213,7 @@ struct dw_controller;
  * @adapter: i2c subsystem adapter node
  * @tx_fifo_depth: depth of the hardware tx fifo
  * @rx_fifo_depth: depth of the hardware rx fifo
+ * @rx_outstanding: current master-rx elements in tx fifo
  * @ss_hcnt: standard speed HCNT value
  * @ss_lcnt: standard speed LCNT value
  * @fs_hcnt: fast speed HCNT value
@@ -257,6 +258,7 @@ struct dw_i2c_dev {
 	u32			master_cfg;
 	unsigned int		tx_fifo_depth;
 	unsigned int		rx_fifo_depth;
+	int			rx_outstanding;
 	int			use_dyn_clk;	/* use dynamic clk setting */
 	u32			clk_khz;	/* input clock */
 	u32			speed_cfg;
