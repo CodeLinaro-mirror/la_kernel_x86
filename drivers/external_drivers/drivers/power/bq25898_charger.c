249b7afbd4d70917fc97feb401a4c7cb23573d2b
Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
2017-03-29 18:59:28 -0700

    em: set VINDPM min threshold to 4100
    as requested by HW team VINDPM min threshold is set
    to 4100, due to device not charging when booting
    with depleted battery
    every VINDPM refresh value has to be:
    max(VBATT+400mV, 4100)
    vindpm is set at boot to 4100
    
    Change-Id: I5a26fae96dab0afcee6498eb0f41e4aa8a2dd70f
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-4614
    Signed-off-by: Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
    Signed-off-by: MorganX Binet <morganx.binet@intel.com>
    Reviewed-on: https://android.intel.com/576046
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Chaumette, HubertX <hubertx.chaumette@intel.com>
    Reviewed-by: Ferrari, AlainX <alainx.ferrari@intel.com>
    Reviewed-by: Dubray, SimonX <simonx.dubray@intel.com>
    Reviewed-by: Whitfield, MichaelX <michaelx.whitfield@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>

--- drivers/external_drivers/drivers/power/bq25898_charger.c
+++ drivers/external_drivers/drivers/power/bq25898_charger.c
@@ -80,6 +80,7 @@
 #define BQ25898_I2C_SLAVE_ADDR			0x6B
 
 #define BQ25898_VINDPM_ADDED_VALUE		400
+#define BQ25898_VINDPM_MIN_VALUE		4100
 #define BQ25898_VINDPM_REG_OFFSET		2600
 #define BQ25898_VINDPM_REG_RATIO		100
 #define BQ25898_VINDPM_TO_REG(x)		(((x) - BQ25898_VINDPM_REG_OFFSET) / BQ25898_VINDPM_REG_RATIO)
@@ -2418,8 +2419,11 @@ static void bq25898_sw_batmon_worker(struct work_struct *work)
 	val = bq25898_rege_convert_uv(ret)/1000;
 
 	/* Set VINDPM */
-	/* VBAT + 400mV */
+	/* max of (VBAT + 400mV) and 4100 */
 	val += BQ25898_VINDPM_ADDED_VALUE;
+	if (val < BQ25898_VINDPM_MIN_VALUE)
+		val = BQ25898_VINDPM_MIN_VALUE;
+
 	/* Write in register (Absolute + Threshold) */
 	dev_info(&chip->client->dev, "writing VINDPM reg0D : 0x%02x\n", BQ25898_VINDPM_TO_REG(val) | FORCE_VINDPM);
 	ret = bq25898_write_reg(chip->client, BQ25898_VIN_CTRL_REG, BQ25898_VINDPM_TO_REG(val) | FORCE_VINDPM);
