43e1f4b98113c565c71add2325db1afb6258edf6
Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
2018-08-29 07:56:39 -0700

    Revert "memcg: add permission check"
    This reverts commit ad0da4e736576d2bbc7208f5ecd349010558282c.
    
    This handler is not defined in struct cgroup_subsys and not part of
    upstream android kernel.
    (checked with HEAD=b4c0be8da99564a1f7f4ee56278039615c7216a0)
    
     Conflicts:
    	mm/memcontrol.c
    
    It seems that this was an Android/Chromium patch, and has been reverted
    in Chromium as well
    
    Links:
    https://android.googlesource.com/kernel/common/+/b4c0be8da99564a1f7f4ee56278039615c7216a0
    https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/394966
    
    Change-Id: I5a8d7ddb715b8be80d4c709a48a03cff1b7373d0
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-7660
    Signed-off-by: Mattijs Korpershoek <mattijsx.korpershoek@intel.com>
    Reviewed-on: https://android.intel.com/643345
    Reviewed-by: jenkins_ndg <jenkins_ndg@intel.com>
    Reviewed-by: Khaldi, RiadhX <riadhx.khaldi@intel.com>
    Reviewed-by: Dubray, SimonX <simonx.dubray@intel.com>
    Reviewed-by: Viel, ClementX <clementx.viel@intel.com>
    Reviewed-by: Ledentec, AlexandreX <alexandrex.ledentec@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5335,7 +5335,6 @@ struct cgroup_subsys memory_cgrp_subsys = {
 	.can_attach = mem_cgroup_can_attach,
 	.cancel_attach = mem_cgroup_cancel_attach,
 	.attach = mem_cgroup_move_task,
-	.allow_attach = mem_cgroup_allow_attach,
 	.post_attach = mem_cgroup_move_task,
 	.bind = mem_cgroup_bind,
 	.dfl_cftypes = memory_files,
