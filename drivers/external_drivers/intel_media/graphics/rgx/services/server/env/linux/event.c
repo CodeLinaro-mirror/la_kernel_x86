4c1d38d2c79db0459672d9c976b89157cc342c3e
Simon Dubray <simonx.dubray@intel.com>
2018-09-19 07:15:00 -0700

    Revert "rgx: use write_lock for event object access"
    This reverts commit cebfa835277088b74db342b816227deabb592f63.
    
    Change-Id: I00b5aa393a201bd0c06415c97c0eed2a0d7b0194
    Tracked-On: https://jira01.devtools.intel.com/browse/AW-8375
    Signed-off-by: Simon Dubray <simonx.dubray@intel.com>
    Reviewed-on: https://android.intel.com/645927
    Reviewed-by: Zaghdoud, WalidX <walidx.zaghdoud@intel.com>
    Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>

--- drivers/external_drivers/intel_media/graphics/rgx/services/server/env/linux/event.c
+++ drivers/external_drivers/intel_media/graphics/rgx/services/server/env/linux/event.c
@@ -268,7 +268,7 @@ PVRSRV_ERROR LinuxEventObjectSignal(IMG_HANDLE hOSEventObjectList)
 	struct list_head *psListEntry, *psListEntryTemp, *psList;
 	psList = &psLinuxEventObjectList->sList;
 
-	write_lock_bh(&psLinuxEventObjectList->sLock);
+	read_lock_bh(&psLinuxEventObjectList->sLock);
 	list_for_each_safe(psListEntry, psListEntryTemp, psList)
 	{
 
@@ -277,7 +277,7 @@ PVRSRV_ERROR LinuxEventObjectSignal(IMG_HANDLE hOSEventObjectList)
 		atomic_inc(&psLinuxEventObject->sTimeStamp);
 		wake_up_interruptible(&psLinuxEventObject->sWait);
 	}
-	write_unlock_bh(&psLinuxEventObjectList->sLock);
+	read_unlock_bh(&psLinuxEventObjectList->sLock);
 
 	return 	PVRSRV_OK;
 
